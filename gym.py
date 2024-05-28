33788cd62f7c31e8b3af7a1d37ab5b7182de97b0
John <wwwjaja1@gmail.com>
2024-05-29 01:06:11 +0800

    20240529
    新增-特定時間進行搶場地

--- gym.py
+++ gym.py
@@ -6,14 +6,14 @@ from selenium.webdriver.chrome.options import Options
 import time
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.common.action_chains import ActionChains
+from datetime import datetime, timedelta
 
 
+URL2 ="https://scr.cyc.org.tw/tp20.aspx?module=net_booking&files=booking_place&StepFlag=2&PT=1&D=2024/06/11&D2=3"
 
-def Conncet_Web_Browers():  # Connect Web Browers
-        
-        ### Chrome ###
-        
+
+def Conncet_Web_Browers(): #瀏覽器條件設定 
+
    
         ## 設定Chrome的瀏覽器彈出時遵照的規則
         ## 這串設定是防止瀏覽器上頭顯示「Chrome正受自動控制」
@@ -49,9 +49,62 @@ def Conncet_Web_Browers():  # Connect Web Browers
         driver = webdriver.Chrome(options=options)
         driver.implicitly_wait(10)  # 设置隐式等待时间
         driver.get("https://scr.cyc.org.tw/tp20.aspx?Module=net_booking&files=booking_before&PT=1")
-        time.sleep(100)
         return driver
 
-Conncet_Web_Browers()
 
 
+
+def wait_until_noon():  #等待到午夜12點
+    now = datetime.now()
+    target_time = now.replace(hour=0, minute=0, second=0, microsecond=0)
+    if now.minute >= 0:
+        target_time += timedelta(days=1)
+    wait_time = (target_time - now).total_seconds()
+    time.sleep(wait_time)
+
+
+
+
+
+def connect_to_website_in_new_tab(driver): #開新分頁連到確認預約資訊
+    # 打开一个新的空白标签页
+    driver.execute_script("window.open('about:blank', '_blank');")
+    # 获取当前窗口句柄
+    current_window_handle = driver.current_window_handle
+    # 切换到新打开的标签页
+    for handle in driver.window_handles:
+        if handle != current_window_handle:
+            driver.switch_to.window(handle)
+            break
+    # 在新标签页中连接到指定的网站
+    driver.get(URL2)
+
+
+def select_time(driver):
+     YuB = driver.find_element(By.XPATH,"/html/body/table[1]/tbody/tr[3]/td/div/form/table/tbody/tr/td/span/div/table/tbody/tr[2]/td/span/table/tbody/tr[5]/td[3]/img")
+     YuB.click()   
+
+
+
+def handle_alert(driver):
+    # 處理弹出窗口
+    try:
+        alert = driver.switch_to.alert
+        print(f"弹出窗口内容: {alert.text}")
+        alert.accept()
+        print("弹出窗口已关闭")
+       
+    except:
+        print("没有检测到弹出窗口")
+     
+
+
+
+
+
+driver = Conncet_Web_Browers()
+wait_until_noon()
+connect_to_website_in_new_tab(driver)
+select_time(driver)
+handle_alert(driver)
+time.sleep(100)
